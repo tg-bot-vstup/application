bcbac96e3e5edf8dbd4ef3d022d26736e6ebe242
brevnoman <dimasautin8@gmail.com>
2022-02-01 20:43:18 +0200

    Updated autoupdate func

--- auto_db_update.py
+++ auto_db_update.py
@@ -14,13 +14,6 @@ async def parse_next_update():
         async with session.get(url="https://vstup.osvita.ua") as response:
             soup = BeautifulSoup(await response.text(), "lxml")
             last_update = soup.find("div", class_="last-update").text.split("(наступне оновлення ")
-            previous = last_update[0].split(' ')[-2].split(".")
-            previous_update_date = datetime.date(
-                year=int(previous[2]),
-                month=int(previous[1]),
-                day=int(previous[0])
-            )
-            print(previous_update_date)
             next = last_update[1].split(")")[0].split(".")
             next_update_date = datetime.datetime(
                 year=int(next[2]),
@@ -29,19 +22,12 @@ async def parse_next_update():
                 hour=23,
                 minute=59
             )
-            return next_update_date, previous_update_date
+            return next_update_date
 
 async def update_one_time_for_a_day():
     while True:
-        next_update, previous_update = await parse_next_update()
-        current_time = datetime.date.today()
-        if current_time > previous_update:
-            """
-            need to add something like table to db containing information about last update
-            """
-            # await get_all_areas_to_db()
-            print("shiiieeeet")
-            print(datetime.date.today())
+        next_update = await parse_next_update()
+        await get_all_areas_to_db()
         pause.until(next_update)
 
 
