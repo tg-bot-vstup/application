6beea118b7f708180af8b73d6f19efbbe9c4fd62
Ubuntu <ubuntu@ip-172-31-24-254.eu-central-1.compute.internal>
2022-02-09 09:08:38 +0000

    added sessionmaker

--- code/db_models.py
+++ code/db_models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, Float, Boolean, create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, sessionmaker
 from dotenv import load_dotenv
 import os
 
