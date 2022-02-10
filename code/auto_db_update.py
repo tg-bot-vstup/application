b6ee7c7e5f4bdcdb1477b2dc6a98e9cc03ccb59a
brevnoman <dimasautin8@gmail.com>
2022-02-10 19:32:37 +0200

    added documentation and typing

--- code/auto_db_update.py
+++ code/auto_db_update.py
@@ -1,33 +1,35 @@
+import asyncio
 import datetime
-import pause
 
 import aiohttp
-import asyncio
-
+import pause
 from bs4 import BeautifulSoup
+from partroller import start_parsing
 
-from partroller import get_areas_dict
 
 async def parse_next_update():
     async with aiohttp.ClientSession() as session:
         async with session.get(url="https://vstup.osvita.ua") as response:
             soup = BeautifulSoup(await response.text(), "lxml")
-            last_update = soup.find("div", class_="last-update").text.split("(наступне оновлення ")
-            next = last_update[1].split(")")[0].split(".")
+            last_update = soup.find("div", class_="last-update").text.split(
+                "(наступне оновлення "
+            )
+            next_update = last_update[1].split(")")[0].split(".")
             next_update_date = datetime.datetime(
-                year=int(next[2]),
-                month=int(next[1]),
-                day=int(next[0]),
+                year=int(next_update[2]),
+                month=int(next_update[1]),
+                day=int(next_update[0]),
                 hour=23,
-                minute=59
+                minute=59,
             )
             return next_update_date
 
+
 async def update_one_time_for_a_day():
     while True:
-        print('Update started')
+        print("Update started")
         next_update = await parse_next_update()
-        await get_areas_dict()
+        await start_parsing()
         pause.until(next_update)
 
 
